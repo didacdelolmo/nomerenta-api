49f226b10f66383b279d508163677de725a5c95b
Didah <didacatz@hotmail.com>
2024-01-20 23:19:55 +0100

    user-controller: Authenticate on register

--- src/controllers/user-controller.mjs
+++ src/controllers/user-controller.mjs
@@ -3,7 +3,9 @@ import * as authService from '../services/auth-service.mjs';
 import { authenticate } from '../utils/auth.mjs';
 
 export async function register(req, res) {
-  res.send(await authService.register(req.body));
+  const user = await authService.register(req.body);
+  authenticate(user, req);
+  res.send();
 }
 
 export async function login(req, res) {
