1e6bf63bffc54484c66ebac7a59dd4b81e1271ca
Didah <didacatz@hotmail.com>
2024-02-01 11:23:54 +0100

    Fix the most retarded bug ever

--- src/controllers/user-controller.mjs
+++ src/controllers/user-controller.mjs
@@ -2,10 +2,14 @@ import * as userService from '../services/user-service.mjs';
 import * as authService from '../services/auth-service.mjs';
 import { authenticate } from '../utils/auth.mjs';
 
+export async function getCurrentUser(req, res) {
+  res.send(await userService.getById(req.session.userId));
+}
+
 export async function register(req, res) {
   const user = await authService.register(req.body);
   authenticate(user, req);
-  res.send();
+  res.send(user);
 }
 
 export async function login(req, res) {
